a5ea8ecca18167e7f976a437bf43f323201b69d7
Asithalak <asithalakmal2500@gmail.com>
2025-08-30 20:06:55 +0530

    relative & absolute

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,15 @@ function App() {
 //screen disign 
   return (
     <div className='w-full h-screen bg-red-100 flex flex-col justify-center items-center'> 
-    <div className='w-[100px] h-[100px] bg-blue-500'> </div>
-    <div className='w-[100px] h-[100px] bg-green-500'></div>
-    <div className='w-[100px] h-[100px] bg-yellow-500'></div>
-    <div className='w-[100px] h-[100px] bg-purple-500'></div>
-    </div>
+      {/* relative for absolute position */}
+      <div className='relative w-[600px] h-[600px] bg-red-500 flex flex-col justify-center items-center' >
+        {/* absolute positioned elements */}
+        <div className='w-[100px] h-[100px] bg-blue-500 absolute  top-[0px] left-[0px]'> </div>
+        <div className='w-[100px] h-[100px] bg-green-500'></div>
+        <div className='w-[100px] h-[100px] bg-yellow-500'></div>
+        <div className='w-[100px] h-[100px] bg-purple-500'></div>
+      </div> 
+    </div> 
   )  
 }
 export default App
