4d437db19c410b2fd4712ec1a778c57798ab24df
Asithalak <asithalakmal2500@gmail.com>
2025-08-30 20:09:05 +0530

    fixed positioned

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,13 @@ function App() {
 //screen disign 
   return (
     <div className='w-full h-screen bg-red-100 flex flex-col justify-center items-center'> 
-      {/* relative for absolute position */}
-      <div className='relative w-[600px] h-[600px] bg-red-500 flex flex-col justify-center items-center' >
-        {/* absolute positioned elements */}
-        <div className='w-[100px] h-[100px] bg-blue-500 absolute  top-[0px] left-[0px]'> </div>
+      <div className='w-[600px] h-[600px] bg-red-500 flex flex-col justify-center items-center' >
+        {/* fixed positioned elements */}
+        <div className='w-[100px] h-[100px] bg-blue-500 fixed top-[0px] left-[0px]'> </div>
         <div className='w-[100px] h-[100px] bg-green-500'></div>
         <div className='w-[100px] h-[100px] bg-yellow-500'></div>
         <div className='w-[100px] h-[100px] bg-purple-500'></div>
-      </div> 
+      </div>  
     </div> 
   )  
 }
