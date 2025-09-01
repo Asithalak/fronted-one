e3ae3bb301a40a2815d614c95afc9bd65f9c7106
Asithalak <asithalakmal2500@gmail.com>
2025-09-02 00:07:49 +0530

    Admin Content

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<Loginpage/>}/>
         <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/admin" element={<AdminPages/>}/>
+        <Route path="/admin/*" element={<AdminPages/>}/>
         <Route path="/*" element={<h1>404 Not Found</h1>}/>
       </Routes>
     </div>
--- src/pages/adminPages.jsx
+++ src/pages/adminPages.jsx
@@ -1,15 +1,20 @@
-import { Link } from "react-router-dom"
+import { Link, Routes, Route} from "react-router-dom"
 export default function AdminPages(){
     return (
-        <div className="w-full h-screen bg-red-700 flex">
-           {/* Sidebar */}
-           <div className="h-full w-[300px] bg-blue-500 flex flex-col">
+        <div className="w-full h-screen  flex">
+         
+           <div className="h-full w-[300px]  flex flex-col">
                 <Link to="/admin/product">Products</Link>
                 <Link to="/admin/category">Categories</Link>
                 <Link to="/admin/user">Users</Link>
            </div>
+           {/* Admin Content */}
            <div className="h-full w-[calc(100%-300px)] bg-yellow-500">
-
+                <Routes path="/*">
+                    <Route path="/product" element={<h1>Product</h1>} />
+                    <Route path="/category" element={<h1>Category</h1>} />
+                    <Route path="/user" element={<h1>User</h1>} />
+                </Routes>
            </div>
         </div>
     )
