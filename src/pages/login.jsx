18c031f5d2bbd6eab3dee46c127124291f88283b
Asithalak <asithalakmal2500@gmail.com>
2025-09-08 00:33:21 +0530

    BackGroun image url

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,8 +1,15 @@
 export default function Loginpage(){
     return (
-        <div className="w-full h-screen bg-red-100px flex flex-col ">
-            <h1 className="font-bold-text-[30px] text-blue-200">crystal beautu clear</h1>
-            <button className=" w-[100px] h-[100px] bg-blue-500 text-white ">Login</button>
+        // bg-[url('/login.jpg')] 
+        <div className="w-full h-screen bg-[url('/login.jpg')] bg-ceter bg-cover flex flex-col justify-center items-center">
+           <div className="w-[50%] h-full ">
+
+           </div>
+           <div className="w-[50%] h-full flex justify-center items-center">
+                <div className="w-[500px] h-[500px] backdrop-blur-md">
+
+                </div>
+           </div>
         </div>
     )
 }
--- src/pages/testPages.jsx
+++ src/pages/testPages.jsx
@@ -1,24 +1,45 @@
 import { useState } from "react"
 
 export default function TestPages(){
-    //hooks function
-   const[a,b]=useState(0) 
-    //a is variable
-    //b is function
+   
+   const[count,setcount]=useState(0) 
+   const[status,setStatus]=useState("Passed") 
+
     return(
 
-           <div className="w-full h-screen  flex justify-center items-center">
+           <div className="w-full h-screen  flex justify-center items-center flex-col">
                 <div className ="w-[450px] h-[250px] shadow flex justify-center items-center "> 
                     <button onClick={() =>{
-                       count = count - 1
+                       setcount(count - 1)
                     }
                 }  className="w-[100px] h-[100px] bg-blue-500 text-white text-center w-[100px] h-[40px] cursor-pointer">-</button>
 
                     <span className="text-[30px] mx-[10px] w-[100px] h-[40px] flex justify-center items-center">{count}</span>
                     <button onClick={() =>{
-                        count = count + 1
+                        setcount(count + 1)
                     }} className="w-[100px] h-[100px] bg-blue-500 text-white text-center w-[100px] h-[40px] text-[20px] cursor-pointer">+</button>
                 </div>
-        </div>
+
+                <div className ="w-[450px] h-[250px] shadow flex flex-col justify-center items-center "> 
+                        <span className="text-[30px] mx-[10px] text-center w-[100px] h-[40px] flex justify-center items-center">
+                            {status}
+                        </span>
+                    <div>   
+                    <button className="w-[100px] h-[100px] bg-blue-500 text-white text-center w-[100px] h-[40px] cursor-pointer m-[10px]" 
+                    onClick={() =>{
+                        setStatus("Passed")
+                    }  }
+                    >Passed</button>
+
+                   
+                    <button  className="w-[100px] h-[100px] bg-blue-500 text-white text-center w-[100px] h-[40px] text-[20px] cursor-pointer m-[10px]" 
+                    onClick={() =>{
+                        setStatus("Failed")
+                    }}>Failed</button>
+
+                    </div> 
+                    </div>
+
+            </div>
     )
 }
