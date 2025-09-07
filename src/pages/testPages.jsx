9de2d4722ccb560511081b01db66930e1f09da10
Asithalak <asithalakmal2500@gmail.com>
2025-09-07 20:57:50 +0530

    hooks usestate

--- src/pages/testPages.jsx
+++ src/pages/testPages.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react"
+
 export default function TestPages(){
-   let count = 0; //use state for count & span 
+    //hooks function
+   const[a,b]=useState(0) 
+    //a is variable
+    //b is function
     return(
 
            <div className="w-full h-screen  flex justify-center items-center">
                 <div className ="w-[450px] h-[250px] shadow flex justify-center items-center "> 
-                    <button className="w-[100px] h-[100px] bg-blue-500 text-white text-center w-[100px] h-[40px]">-</button>
+                    <button onClick={() =>{
+                       count = count - 1
+                    }
+                }  className="w-[100px] h-[100px] bg-blue-500 text-white text-center w-[100px] h-[40px] cursor-pointer">-</button>
 
-                    <span className="text-[30px] mx-[10px]">{count}</span>
-                    <button className="w-[100px] h-[100px] bg-blue-500 text-white text-center w-[100px] h-[40px] text-[20px]">+</button>
+                    <span className="text-[30px] mx-[10px] w-[100px] h-[40px] flex justify-center items-center">{count}</span>
+                    <button onClick={() =>{
+                        count = count + 1
+                    }} className="w-[100px] h-[100px] bg-blue-500 text-white text-center w-[100px] h-[40px] text-[20px] cursor-pointer">+</button>
                 </div>
         </div>
     )
