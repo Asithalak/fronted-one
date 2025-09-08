230177b84b4b5eee14d7c8cfcb7122e03a221d81
Asithalak <asithalakmal2500@gmail.com>
2025-09-08 19:21:56 +0530

    login form

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,13 +1,18 @@
 export default function Loginpage(){
     return (
-        // bg-[url('/login.jpg')] 
+        
         <div className="w-full h-screen bg-[url('/login.jpg')] bg-ceter bg-cover flex flex-col justify-center items-center">
            <div className="w-[50%] h-full ">
 
            </div>
            <div className="w-[50%] h-full flex justify-center items-center">
-                <div className="w-[500px] h-[500px] backdrop-blur-md">
-
+                <div className="w-[500px] h-[600px] backdrop-blur-md rounded-[20px] shadow-xl flex flex-col justify-center items-center">
+                    // Login Form
+                    <input className="w-[300px] h-[50px]  border border-[#c3efe9] rounded-[20px] mb-[20px]"></input>
+                    // Password Form
+                    <input type="password" className="w-[300px] h-[50px]  border border-[#c3efe9] rounded-[20px] mb-[10px]"></input>
+                    // Remember Me Checkbox
+                    <button className="w-[300px] h-[50px] bg-[#c3efe9] rounded-[20px] mt-10">Login</button>
                 </div>
            </div>
         </div>
