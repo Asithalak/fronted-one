7cab572f504f9c076e74aaf028f54d60f9d69b73
Asithalak <asithalakmal2500@gmail.com>
2025-09-25 22:19:28 +0530

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Loginpage from './pages/login'
 import HomePage from './pages/home'
-import SignUp from './pages/signup'
+import Registerpage from './pages/register'
 import AdminPages from './pages/adminPages'
 import TestPages from './pages/testPages'
 import { Toaster } from 'react-hot-toast'
-
+import AdminProductPage from './pages/admin/adminProductPage'
 function App() {
  
  
@@ -17,10 +17,11 @@ function App() {
       {/* <Header/> */}
       <Routes path="/*">
         <Route path="/" element={<HomePage/>}/>
-        <Route path="/login" element={<Loginpage/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/login/" element={<Loginpage/>}/>
         <Route path="/admin/*" element={<AdminPages/>}/>
+        <Route path="/signup" element={<Registerpage/>}/>
         <Route path="/test" element={<TestPages/>}/>
+        <Route path="/admin/product" element={<AdminProductPage/>}/>
         <Route path="/*" element={<h1>404 Not Found</h1>}/>
       </Routes>
     </div>
