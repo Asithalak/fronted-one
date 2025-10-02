50565d5241124fbd7b8bc400fdda05e46bb3ee02
Asithalak <asithalakmal2500@gmail.com>
2025-10-03 00:38:53 +0530

    table row and table data

--- src/pages/admin/adminProductPage.jsx
+++ src/pages/admin/adminProductPage.jsx
@@ -18,9 +18,22 @@ export default function AdminProductPage(){
                     </tr>
                 </thead> 
                 <tbody>
-                    {
-                        //jsx thula js use kirumata tbody ek thula {} use karanna one
-                    }
+                  {
+                    products.map((items,index)=>{ 
+                        return (//use key function and table row and table data
+                            <tr key={index}>
+                                <td>{items.productId}</td>
+                                <td>{items.name}</td>
+                                <td>
+                                    <img src={items.images[0]} className="w-[100px] h-[100px] object-cover"/>
+                                </td>
+                                <td>{items.labelledPrice}</td>
+                                <td>{items.price}</td>
+                                <td>{items.stock}</td> 
+                            </tr>
+                          )
+                    })
+                  }
                 </tbody>
                 </table> 
         </div >
